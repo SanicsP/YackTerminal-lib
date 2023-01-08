13d9d1e1797094f320f530983a3bb735bcefe813
Sanics'P <sanics290@gmail.com>
2023-01-08 22:00:08 +0100

    déclaration de constructeurs dans la classe Field_wrapper

--- YT_Library/header/YT/Field_Wrapper.hpp
+++ YT_Library/header/YT/Field_Wrapper.hpp
@@ -11,7 +11,20 @@
 
 		class Field_Wrapper : public Command_Wrapper
 		{
-			
+			public : //constructors 
+				
+				Field_Wrapper(const FieldParser& fp , const FieldExecute& fe,
+							 const std::string& err_msg , const std::string& rn_msg
+				);
+
+				Field_Wrapper(const Field_Wrapper& other);
+
+			public : //operations
+				//inherited
+				bool execute() const override;
+			private : 
+				FieldParser m_parser_func;
+				FieldExecute m_execute_func;
 		};
 
 	}
